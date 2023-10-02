237bbc6f4a8b1dbfce34acdd2c8948e50db4d913
aFy4a <afy4a@vk.com>
2023-10-02 20:37:32 +0300

    переименовал файл с тестами

---
+++ tests/test_sort.py
@@ -0,0 +1,17 @@
+from sort import sort
+
+
+def test_empty():
+    assert sort([]) == []
+
+
+def test_more_than_5():
+    assert sort([6, 7, 8, 6, 10, 6]) == []
+
+
+def test_less_than_5():
+    assert sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+
+
+def test():
+    assert sort([1, 5, 6, 4, 9, 2, 8]) == [1, 5, 4, 2]
