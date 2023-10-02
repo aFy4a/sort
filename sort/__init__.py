93a5599a11bb659b4a9c91f3a2eaa883ed5a7550
aFy4a <afy4a@vk.com>
2023-10-02 20:36:52 +0300

    исправил ввод

--- sort/__init__.py
+++ sort/__init__.py
@@ -1,17 +1,15 @@
 import click
 
-@click.command()
-@click.option("--arr", help="array of integers.")
+
 def sort(arr):
     temp = []
     for i in range(len(arr)):
         if arr[i] < 6:
-            temp += arr[i]
+            temp.append(arr[i])
 
     return temp
 
 @click.command()
-def main():
-    arr = list(map(int, input().split()))
-
+@click.option("--arr", help="array of integers.", multiple=True, type=int)
+def main(arr):
     click.echo(sort(arr))
